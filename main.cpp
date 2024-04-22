e19a019662bf312c416e26f45baeaa57124ebe0c
dominikswies <dswies@student.agh.edu.pl>
2024-04-22 18:37:28 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float pierw(float liczba)
+{
+		
+	return sqrt(liczba);
+}
+
+float pierw(float liczba, float stopien)
+{
+
+	return pow(liczba,1/stopien);
+}
+
+int main() {
+
+
+for(int i = 1;i<=10;i++)
+{
+	cout << "Pierwiastek kwadratowy z: " << i << " = " << pierw(float(i)) << endl;
+	cout << "Pierwiastek trzeciego stopnia z: " << i << " = " << pierw(float(i),3) << endl;
+}
+
+	return 0;
+}
